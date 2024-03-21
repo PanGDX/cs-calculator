b7b80ccdf33298cb8a0685ba49756424222c1d70
Pran <prtanprasertkul@gmail.com>
2024-03-21 15:20:35 +0700

    Added labelling and catching minor errors

--- main.py
+++ main.py
@@ -16,24 +16,34 @@ class CalculatorApp(tk.Tk):
 
 
         search_frame = Frame(self.root)
-        search_frame.pack(pady = "10px")
+        search_frame.pack(pady = "5px")
 
         self.search_var = tk.StringVar()
         self.search_var.trace_add("write", self.update_list)
 
+        search_message = tk.Label(search_frame, text="Search Here",font=("Helvetica 13"))
+        search_message.pack(pady="5px")
+
         self.entry = tk.Entry(search_frame, textvariable=self.search_var, font=("Helvetica 13"), width = 30)
-        self.entry.pack(padx = "15px")
+        self.entry.pack(pady = "10px")
 
         self.listbox = tk.Listbox(search_frame, font=("Helvetica 13"), height = 8, width = 40)
         self.listbox.pack()
 
-        self.items = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew"]
+        self.items = [
+            "Lagrange Multiplier", 
+            "Partial Differentiation", 
+            "Multi-variable integration", 
+            "Chain Rule", 
+            "curl", 
+            "div", 
+        ]
         self.update_list()
 
         button_frame = Frame(self.root)
         button_frame.pack(side=BOTTOM, pady="10px", padx="10px")
 
-        run_calculation_button = Button(button_frame, text="Run Calculation", command=self.run_calculation, width=15, height=2, font=("Helvetica 13"))
+        run_calculation_button = Button(button_frame, text="Select Operation", command=self.run_calculation, width=15, height=2, font=("Helvetica 13"))
         run_calculation_button.pack(pady="10px", padx="5px")
 
         self.root.bind("<Control-c>", self.copy_latex)
@@ -52,8 +62,12 @@ class CalculatorApp(tk.Tk):
         matplotlib_frame = Frame(self.root)
         matplotlib_frame.pack(side=tk.TOP, padx="5px", fill="both")
 
+        entry_box_message = tk.Label(matplotlib_frame,text = "Input LaTex Here (Enter to input and check for errors)", font=("Helvetica 13"))
+        entry_box_message.pack(pady="2px")
+
+
         self.latex_entry = Entry(matplotlib_frame, width=70, font=("Helvetica 13"))
-        self.latex_entry.pack(side=tk.TOP, pady="30px", padx="150px", fill="x")
+        self.latex_entry.pack(side=tk.TOP, pady="15px", padx="150px", fill="x")
 
         fig = matplotlib.figure.Figure(figsize=(10, 3), dpi=100)
         self.wx = fig.add_subplot(111)
@@ -75,20 +89,25 @@ class CalculatorApp(tk.Tk):
 
     def graph(self, event):
         tmptext = self.latex_entry.get()
-        tmptext = "$" + tmptext + "$"
-        self.wx.clear()
-        try:
-            self.wx.text(0.5, 0.5, tmptext, fontsize=13, ha='center', va='center')
+        if tmptext == "":
+            self.wx.clear()
             self.latex_canvas.draw()
-        except ValueError as e:
-            messagebox.showerror("Error", f"Error while parsing the mathematical expression {e}!\n Possibly incorrect Latex format!")
+        else:
+            tmptext = "$" + tmptext + "$"
+            self.wx.clear()
+            try:
+                self.wx.text(0.5, 0.5, tmptext, fontsize=13, ha='center', va='center')
+                self.latex_canvas.draw()
+            except ValueError as e:
+                messagebox.showerror("Error", f"Error while parsing the mathematical expression {e}!\n Possibly incorrect Latex format!")
 
 if __name__ == '__main__':
     root = tk.Tk()
-    app_width = 1400
-    app_height = 700
+    width= root.winfo_screenwidth() 
+    height= root.winfo_screenheight()
+
+    root.state('zoomed')
     root.title("ULTIMATE CALCULATOR")
-    root.geometry(f"{app_width}x{app_height}")
 
     app = CalculatorApp(root)
     root.mainloop()
